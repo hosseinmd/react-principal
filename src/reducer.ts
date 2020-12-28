16881933caf7673261476e958957488d1c2377e3
Hossein Mohammadi <hosseinm.developer@gmail.com>
2020-12-28 12:09:26 +0330

    fix: reducer type

--- src/reducer.ts
+++ src/reducer.ts
@@ -8,17 +8,12 @@ type Handlers<STATE, ACTION = HandlerAction<STATE>> = {
   [type: string]: Reducer<STATE, ACTION>;
 };
 
-type CreateReducer = <
-  STATE = any,
-  ACTION extends HandlerAction<any> = HandlerAction<STATE>,
->(
-  handlers: Handlers<STATE, ACTION>,
-) => Reducer<STATE, ACTION>;
+type CreateReducer = <STATE = any>(handlers: Handlers<STATE>) => Reducer<STATE>;
 
 export const createReducer: CreateReducer = (handlers) => (state, action) => {
   if (!handlers.hasOwnProperty(action.type)) {
     return state;
   }
 
-  return handlers[action.type](state, action);
+  return handlers[action.type](state, action as any);
 };
